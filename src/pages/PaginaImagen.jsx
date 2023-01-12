9904c27f7be321d2f7f4c64642249dcd89b8b108
Enloch <Hugodlcq@gmail.com>
2023-01-12 21:54:22 +0100

    Revision

--- src/pages/PaginaImagen.jsx
+++ src/pages/PaginaImagen.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import imagen from '../assets/Dolker-Completo.png';
+import imagen1 from '../assets/Dolker-Completo-01.webp';
+import imagen2 from '../assets/Dolker-Completo-02.webp';
+import imagen3 from '../assets/Dolker-Completo-03.webp';
+import imagen4 from '../assets/Dolker-Completo-04.webp';
+import imagen5 from '../assets/Dolker-Completo-05.webp';
 const PaginaImagenes = () => {
   return (
     <>
       <div>
-        <img src={imagen} />
+        <img src={imagen1} />
+        <img src={imagen2} />
+        <img src={imagen3} />
+        <img src={imagen4} />
+        <img src={imagen5} />
       </div>
     </>
   )
