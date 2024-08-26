2d3e64fe504c8411029aa48a5d58112140b50183
Enloch <hugodlcq@gmail.com>
2024-08-26 17:36:09 +0200

    Nomenclatura

--- src/configs/SeriesMatrix/configPlanex.jsx
+++ src/configs/SeriesMatrix/configPlanex.jsx
@@ -1,35 +1,35 @@
-import Planex3a from "../../assets/images/DolckerMatrix/Series/Planex/matriz1_a.jpg";
-import Planex3b from "../../assets/images/DolckerMatrix/Series/Planex/matriz1_b.jpg";
-import Planex3c from "../../assets/images/DolckerMatrix/Series/Planex/matriz1_c.jpg";
-import Planex3d from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_a.jpg";
-import Planex3e from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_b.jpg";
-import Planex3f from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_c.jpg";
-import Planex3g from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_d.jpg";
-import Planex3h from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_e.jpg";
-import Planex3i from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_f.jpg";
-import Planex3j from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_g.jpg";
-import Planex3k from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_h.jpg";
-import Planex3l from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_i.jpg";
-import Planex3m from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_j.jpg";
-import Planex3n from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_k.jpg";
-import Planex3o from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_l.jpg";
-import Planex3p from "../../assets/images/DolckerMatrix/Series/Planex/matriz2_m.jpg";
+import Planex3a from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz1_a.jpg';
+import Planex3b from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz1_b.jpg';
+import Planex3c from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz1_c.jpg';
+import Planex3d from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_a.jpg';
+import Planex3e from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_b.jpg';
+import Planex3f from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_c.jpg';
+import Planex3g from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_d.jpg';
+import Planex3h from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_e.jpg';
+import Planex3i from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_f.jpg';
+import Planex3j from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_g.jpg';
+import Planex3k from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_h.jpg';
+import Planex3l from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_i.jpg';
+import Planex3m from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_j.jpg';
+import Planex3n from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_k.jpg';
+import Planex3o from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_l.jpg';
+import Planex3p from '../../assets/images/DolckerMatrix/Series/PLANEX/matriz2_m.jpg';
 
 export const configPlanex = [
-  { id: "1", img: Planex3a, title: "Planex A" },
-  { id: "2", img: Planex3b, title: "Planex B" },
-  { id: "3", img: Planex3c, title: "Planex C" },
-  { id: "4", img: Planex3d, title: "Planex D" },
-  { id: "5", img: Planex3e, title: "Planex E" },
-  { id: "6", img: Planex3f, title: "Planex F" },
-  { id: "7", img: Planex3g, title: "Planex G" },
-  { id: "8", img: Planex3h, title: "Planex H" },
-  { id: "9", img: Planex3i, title: "Planex I" },
-  { id: "10", img: Planex3j, title: "Planex J" },
-  { id: "11", img: Planex3k, title: "Planex K" },
-  { id: "12", img: Planex3l, title: "Planex L" },
-  { id: "13", img: Planex3m, title: "Planex M" },
-  { id: "14", img: Planex3n, title: "Planex N" },
-  { id: "15", img: Planex3o, title: "Planex O" },
-  { id: "16", img: Planex3p, title: "Planex P" },
+  { id: '1', img: Planex3a, title: 'Planex A' },
+  { id: '2', img: Planex3b, title: 'Planex B' },
+  { id: '3', img: Planex3c, title: 'Planex C' },
+  { id: '4', img: Planex3d, title: 'Planex D' },
+  { id: '5', img: Planex3e, title: 'Planex E' },
+  { id: '6', img: Planex3f, title: 'Planex F' },
+  { id: '7', img: Planex3g, title: 'Planex G' },
+  { id: '8', img: Planex3h, title: 'Planex H' },
+  { id: '9', img: Planex3i, title: 'Planex I' },
+  { id: '10', img: Planex3j, title: 'Planex J' },
+  { id: '11', img: Planex3k, title: 'Planex K' },
+  { id: '12', img: Planex3l, title: 'Planex L' },
+  { id: '13', img: Planex3m, title: 'Planex M' },
+  { id: '14', img: Planex3n, title: 'Planex N' },
+  { id: '15', img: Planex3o, title: 'Planex O' },
+  { id: '16', img: Planex3p, title: 'Planex P' },
 ];
