9ab279dcb4fc8cd72ad1b3aef2c7fd52867ce664
Enloch <Hugodlcq@gmail.com>
2025-02-12 12:07:28 +0100

    Falta SVG + Detalle

--- src/modules/Matrix/04_Aplicaciones/index.jsx
+++ src/modules/Matrix/04_Aplicaciones/index.jsx
@@ -12,92 +12,92 @@ import Cita from "../../../components/Cita";
 import ImageWithCaptionSlider from "../../../components/ImageWithCaptionSlider";
 
 const AplicacionesTline = ({ id }) => {
-  return (
-    <>
-      <StyledAplicaciones
-        id={id[0]}
-        backgroundColor={COLORS.gray02}
-        rowGap={DISTANCES.small}
-      >
-        <Titulo color={COLORS.gray08}>Aplicaciones</Titulo>
-        <ImageWithCaption
-          src={img1}
-          alt='Imagen de Aplicaciones 1'
-          caption=''
-          columnSpan='6'
-        />
-        <Cita
-          cita='Los detalles no son los detalles. Los detalles son el diseño.'
-          author='Charles Eames '
-          colorCita={COLORS.gray08}
-          colorAutor={COLORS.gray04}
-        />
-      </StyledAplicaciones>
-      <StyledAplicaciones
-        id={id[1]}
-        backgroundColor={COLORS.gray01}
-        rowGap={DISTANCES.small}
-      >
-        <Titulo2 color={COLORS.gray08}>Rehabilitación</Titulo2>
-        <ImageWithCaption
-          src={logos}
-          alt='Imagen de Aplicaciones 1'
-          caption=''
-          columnSpan='6'
-        />
-        <Cita
-          cita='Lo funcional es mejor que lo bello, porque lo que funciona bien permanece en el tiempo.'
-          author='Ray Eames'
-          colorCita={COLORS.gray08}
-          colorAutor={COLORS.gray04}
-        />
-      </StyledAplicaciones>
-      <StyledAplicaciones
-        id={id[2]}
-        backgroundColor={COLORS.gray01}
-        rowGap={DISTANCES.small}
-      >
-        <Titulo2 color={COLORS.gray08}>Obra nueva</Titulo2>
-        <ImageWithCaption
-          src={img3}
-          alt='Imagen de Aplicaciones 1'
-          //   caption='Jorge dará la referencia o imágen.'
-          columnSpan='6'
-        />
-        <Cita
-          cita='El espacio no tiene sentido sin luz. Un edificio habla a través del silencio de la percepción orquestada por la luz.'
-          author='Steven Holl'
-          colorCita={COLORS.gray08}
-          colorAutor={COLORS.gray04}
-        />
-      </StyledAplicaciones>
-      <StyledAplicaciones
-        id={id[3]}
-        backgroundColor={COLORS.gray01}
-        rowGap={DISTANCES.small}
-      >
-        <Titulo2 color={COLORS.gray08}>Interiores</Titulo2>
-        <ImageWithCaptionSlider
-          images={[
-            {
-              src: img4,
-              alt: "Imagen de Aplicaciones 4",
-              caption:
-                "La originalidad consiste en el retorno al origen; así pues, original es aquello que vuelve a la simplicidad de las primeras soluciones”. Antonio Gaudí.",
-            },
-            {
-              src: img5, // Asegúrate de definir img5 en alguna parte de tu código
-              alt: "Imagen de Aplicaciones 5",
-              caption:
-                "La originalidad consiste en el retorno al origen; así pues, original es aquello que vuelve a la simplicidad de las primeras soluciones”. Antonio Gaudí.",
-            },
-            // ... puedes agregar más imágenes aquí
-          ]}
-          columnSpan='6'
-        />
-      </StyledAplicaciones>
-    </>
-  );
+	return (
+		<>
+			<StyledAplicaciones
+				id={id[0]}
+				backgroundColor={COLORS.gray02}
+				rowGap={DISTANCES.small}
+			>
+				<Titulo color={COLORS.gray08}>Aplicaciones</Titulo>
+				<ImageWithCaption
+					src={img1}
+					alt="Imagen de Aplicaciones 1"
+					caption=""
+					columnSpan="6"
+				/>
+				<Cita
+					cita="Los detalles no son los detalles. Los detalles son el diseño."
+					author="Charles Eames "
+					colorCita={COLORS.gray08}
+					colorAutor={COLORS.gray04}
+				/>
+			</StyledAplicaciones>
+			<StyledAplicaciones
+				id={id[1]}
+				backgroundColor={COLORS.gray01}
+				rowGap={DISTANCES.small}
+			>
+				<Titulo2 color={COLORS.gray08}>Rehabilitación</Titulo2>
+				<ImageWithCaption
+					src={logos}
+					alt="Imagen de Aplicaciones 1"
+					caption=""
+					columnSpan="6"
+				/>
+				<Cita
+					cita="Lo funcional es mejor que lo bello, porque lo que funciona bien permanece en el tiempo."
+					author="Ray Eames"
+					colorCita={COLORS.gray08}
+					colorAutor={COLORS.gray04}
+				/>
+			</StyledAplicaciones>
+			<StyledAplicaciones
+				id={id[2]}
+				backgroundColor={COLORS.gray01}
+				rowGap={DISTANCES.small}
+			>
+				<Titulo2 color={COLORS.gray08}>Obra nueva</Titulo2>
+				<ImageWithCaption
+					src={img3}
+					alt="Imagen de Aplicaciones 1"
+					//   caption='Jorge dará la referencia o imágen.'
+					columnSpan="6"
+				/>
+				<Cita
+					cita="El espacio no tiene sentido sin luz. Un edificio habla a través del silencio de la percepción orquestada por la luz."
+					author="Steven Holl"
+					colorCita={COLORS.gray08}
+					colorAutor={COLORS.gray04}
+				/>
+			</StyledAplicaciones>
+			<StyledAplicaciones
+				id={id[3]}
+				backgroundColor={COLORS.gray01}
+				rowGap={DISTANCES.small}
+			>
+				<Titulo2 color={COLORS.gray08}>Exteriores</Titulo2>
+				<ImageWithCaptionSlider
+					images={[
+						{
+							src: img4,
+							alt: "Imagen de Aplicaciones 4",
+							caption:
+								"La originalidad consiste en el retorno al origen; así pues, original es aquello que vuelve a la simplicidad de las primeras soluciones”. Antonio Gaudí.",
+						},
+						{
+							src: img5, // Asegúrate de definir img5 en alguna parte de tu código
+							alt: "Imagen de Aplicaciones 5",
+							caption:
+								"La originalidad consiste en el retorno al origen; así pues, original es aquello que vuelve a la simplicidad de las primeras soluciones”. Antonio Gaudí.",
+						},
+						// ... puedes agregar más imágenes aquí
+					]}
+					columnSpan="6"
+				/>
+			</StyledAplicaciones>
+		</>
+	);
 };
 
 export default AplicacionesTline;
--- src/pages/catalogos/DolckerLeiva.jsx
+++ src/pages/catalogos/DolckerLeiva.jsx
@@ -2,87 +2,93 @@ import React, { useRef, useState, useEffect } from "react";
 import MultipleOptionsProvider from "../../contexts/MultipleOptionsContext";
 import styled from "styled-components";
 import {
-  AplicacionesLeiva as Aplicaciones,
-  CaracteristicasLeiva as Caracteristicas,
-  DescargasLeiva as Descargas,
-  DisenosLeiva as Disenos,
-  DolckerLeiva as Dolcker,
-  Final,
-  IndiceLeiva as Indice,
-  SistemasLeiva as Sistemas,
-  VentajasLeiva as Ventajas,
+	AplicacionesLeiva as Aplicaciones,
+	CaracteristicasLeiva as Caracteristicas,
+	DescargasLeiva as Descargas,
+	DisenosLeiva as Disenos,
+	DolckerLeiva as Dolcker,
+	Final,
+	IndiceLeiva as Indice,
+	SistemasLeiva as Sistemas,
+	VentajasLeiva as Ventajas,
 } from "../../modules";
 
 const DolckerLeiva = () => {
-  const indiceRef = useRef();
-  const [showButton, setShowButton] = useState(false);
+	const indiceRef = useRef();
+	const [showButton, setShowButton] = useState(false);
 
-  const handleGoToIndice = () => {
-    indiceRef.current.scrollIntoView({ behavior: "smooth" });
-  };
+	const handleGoToIndice = () => {
+		indiceRef.current.scrollIntoView({ behavior: "smooth" });
+	};
 
-  const handleScroll = () => {
-    const scrollTop = window.pageYOffset;
-    const dolckerOffsetTop = document.getElementById("dolcker").offsetTop;
-    setShowButton(scrollTop > dolckerOffsetTop);
-  };
+	const handleScroll = () => {
+		const scrollTop = window.pageYOffset;
+		const dolckerOffsetTop = document.getElementById("dolcker").offsetTop;
+		setShowButton(scrollTop > dolckerOffsetTop);
+	};
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  return (
-    <MultipleOptionsProvider>
-      <Indice id='indice' indiceRef={indiceRef} />
-      <Dolcker id='dolcker' />
-      <Ventajas id='ventajas' />
-      <Aplicaciones
-        id={["aplicaciones", "rehabilitacion", "obra-nueva", "interiores"]}
-      />
-      <Disenos id={["diseno", "colecciones", "configurador"]} />
-      <Caracteristicas id={["caracteristicas-especiales", "datos-tecnicos"]} />
-      <Sistemas
-        id={[
-          "sistemas",
-          "dolcker-and-clip",
-          "dol-tc11",
-          "dol-tc14",
-          "dol-hc20",
-        ]}
-      />
-      {/* <Descargas id='descargas' /> */}
-      <Final />
-      {showButton && <StyledButton onClick={handleGoToIndice}>^</StyledButton>}
-    </MultipleOptionsProvider>
-  );
+	return (
+		<MultipleOptionsProvider>
+			<Indice id="indice" indiceRef={indiceRef} />
+			<Dolcker id="dolcker" />
+			<Ventajas id="ventajas" />
+			<Aplicaciones
+				id={["aplicaciones", "rehabilitacion", "obra-nueva", "interiores"]}
+			/>
+			<Disenos id={["diseno", "colecciones", "configurador"]} />
+			<Caracteristicas id={["caracteristicas-especiales", "datos-tecnicos"]} />
+			<Sistemas
+				id={[
+					"sistemas",
+					"dolcker-and-clip",
+					"dol-tc11",
+					"dol-tc14",
+					"dol-hc20",
+				]}
+			/>
+			{/* <Descargas id='descargas' /> */}
+			<Final />
+			{showButton && (
+				<StyledButton onClick={handleGoToIndice}>
+					<span role="img" aria-label="up arrow">
+						&#8593;
+					</span>
+				</StyledButton>
+			)}
+		</MultipleOptionsProvider>
+	);
 };
 
 export default DolckerLeiva;
 
 const StyledButton = styled.button`
-  position: fixed;
-  bottom: 20px;
-  right: 20px;
-  background-color: #bfbfbf1c;
-  color: #000000;
-  border: none;
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  font-size: 24px;
-  font-weight: bold;
-  text-align: center;
-  line-height: 50px;
-  cursor: pointer;
-  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
-  transition: all 0.3s ease-in-out;
+	position: fixed;
+	bottom: 20px;
+	right: 20px;
+	background-color: #bfbfbf1c;
+	color: #000000;
+	border: none;
+	border-radius: 50%;
+	width: 25px;
+	height: 25px;
+	font-size: 16px;
+	font-weight: bold;
+	text-align: center;
+	line-height: 25px;
+	cursor: pointer;
+	box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
+	transition: all 0.3s ease-in-out;
 
-  &:hover {
-    background-color: lightgray;
-    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
-    transform: scale(1.1);
-  }
+	&:hover {
+		background-color: lightgray;
+		box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
+		transform: scale(1.1);
+	}
 `;
