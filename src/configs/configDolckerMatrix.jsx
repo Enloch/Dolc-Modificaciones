a94d25b30ad9719ea4881ed133e924ff153fdea2
Enloch <Hugodlcq@gmail.com>
2025-02-14 12:27:38 +0100

    Cambio imagen inicio y matrix facade

--- src/configs/configDolckerMatrix.jsx
+++ src/configs/configDolckerMatrix.jsx
@@ -3,54 +3,54 @@ const piezasImport = import.meta.globEager("../assets/icons/Matrix/piezas/**");
 const piezas = getDataFromModules(piezasImport);
 
 const config = [
-  {
-    top: "20%",
-    left: "18%",
-    content: {
-      imgSrc: piezas[0],
-      title: "Coronación",
-    },
-  },
-  {
-    top: "23%",
-    left: "76%",
-    content: {
-      imgSrc: piezas[1],
-      title: "Esquina A",
-    },
-  },
-  //   {
-  //     top: "70.3%",
-  //     left: "77.3%",
-  //     content: {
-  //       imgSrc: piezas[2],
-  //       title: "Esquina b",
-  //     },
-  //   },
-  {
-    top: "66%",
-    left: "16%",
-    content: {
-      imgSrc: piezas[3],
-      title: "LED",
-    },
-  },
-  {
-    top: "48%",
-    left: "31%",
-    content: {
-      imgSrc: piezas[4],
-      title: "Recerco",
-    },
-  },
-  {
-    top: "73%",
-    left: "60%",
-    content: {
-      imgSrc: piezas[5],
-      title: "Rejilla",
-    },
-  },
+	{
+		top: "20%",
+		left: "39%",
+		content: {
+			imgSrc: piezas[0],
+			title: "Coronación",
+		},
+	},
+	{
+		top: "38%",
+		left: "47%",
+		content: {
+			imgSrc: piezas[1],
+			title: "Esquina A",
+		},
+	},
+	//   {
+	//     top: "70.3%",
+	//     left: "77.3%",
+	//     content: {
+	//       imgSrc: piezas[2],
+	//       title: "Esquina b",
+	//     },
+	//   },
+	{
+		top: "28%",
+		left: "75%",
+		content: {
+			imgSrc: piezas[3],
+			title: "LED",
+		},
+	},
+	{
+		top: "30%",
+		left: "58%",
+		content: {
+			imgSrc: piezas[4],
+			title: "Recerco",
+		},
+	},
+	{
+		top: "47%",
+		left: "78%",
+		content: {
+			imgSrc: piezas[5],
+			title: "Rejilla",
+		},
+	},
 ];
 
 export default config;
