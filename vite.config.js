7f549550bd8670158972d8b954be2646572491f8
Enloch <Hugodlcq@gmail.com>
2025-02-21 13:16:26 +0100

    aa

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [
 		react({
 			// Add this line
-			include: "**/*.{jsx,svg,png,jpg,gif}",
+			include: "**/*.{jsx,svg,png,jpg,gif,webp}",
 		}),
 	],
 	server: {
