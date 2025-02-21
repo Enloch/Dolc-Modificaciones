224faec25c7f6bbde610723a08448ad91d8ae6ac
Enloch <Hugodlcq@gmail.com>
2025-02-21 08:30:56 +0100

    Añadidas galerias y reemplazo de imagenes

--- src/modules/T5/02_Dolcker/index.jsx
+++ src/modules/T5/02_Dolcker/index.jsx
@@ -1,91 +1,93 @@
-import ImageWithCaption from '../../../components/ImageWithCaption';
-import { Titulo } from '../../../components/Titulos';
-import StyledDolcker from './styles';
-import { COLORS } from '../../../global/GlobalStyles';
+import ImageWithCaption from "../../../components/ImageWithCaption";
+import { Titulo } from "../../../components/Titulos";
+import StyledDolcker from "./styles";
+import { COLORS } from "../../../global/GlobalStyles";
 
-import img2 from '../../../assets/images/Dolcker/catalogo3-1-5.webp';
-import img3 from '../../../assets/images/T5/Dolcker/catalogo3-1-7.jpg';
-import VisualizadorNota from '../../../components/VisualizadorNota';
-import Text from '../../../components/Text';
+import img2 from "../../../assets/images/Dolcker/catalogo3-1-5.webp";
+import img3 from "../../../assets/images/T5/Dolcker/catalogo3-1-7.jpg";
+import VisualizadorNota from "../../../components/VisualizadorNota";
+import Text from "../../../components/Text";
 
-import config from '../../../configs/configDolcker';
+import config from "../../../configs/configDolcker";
 
 const Dolcker = ({ id }) => {
-  const propsVisualizador = {
-    text: 'Pasa el ratón por los puntos para ver la información',
-    type: 'normal',
-    // indicadores: config,
-    imgBackground: img3,
-    alt: 'Imagen de fondo con indicadores',
-  };
+	const propsVisualizador = {
+		text: "Pasa el ratón por los puntos para ver la información",
+		type: "normal",
+		// indicadores: config,
+		imgBackground: img3,
+		alt: "Imagen de fondo con indicadores",
+	};
 
-  return (
-    <StyledDolcker id={id} backgroundColor={COLORS.gray03}>
-      <Titulo>Dolcker T5 soluciona</Titulo>
-      <div className='dolcker-info'>
-        <ImageWithCaption
-          src={img2}
-          alt='Imagen Dolcker 2'
-          caption='Sistema T5.
-'
-        />
-        <Text>
-          Dolcker T5 XL Porcelánico: innovación y diseño para fachadas ventiladas de última
-          generación.
-          <br />
-          <br />
-          El <strong>Sistema DOL-T5 XL</strong> es una solución avanzada para fachadas
-          ventiladas, diseñada para la instalación de revestimientos cerámicos de gran
-          formato con un sistema de fijación híbrido, combinando grapa oculta y adhesivo
-          estructural. Su diseño optimizado garantiza un montaje ligero, seguro y eficiente,
-          utilizando materiales de la más alta calidad. Características Principales
-          <br />
-          <br />
-          <strong>Sistema de fijación:</strong>
-          <br />
-          Grapa oculta para una estética limpia y uniforme.
-          <br />
-          Compatible con cerámica de 3,5 - 5,6 - 10,5 - 12 mm de espesor.
-          <br />
-          Juntas horizontales de 5-6 mm y juntas verticales de 2 mm o más, asegurando
-          estabilidad y estética.
-          <br />
-          <br />
-          <strong>Anclaje y perfilería:</strong>
-          <br />
-          Ménsulas simples y dobles en L de aluminio para una fijación robusta.
-          <br />
-          Perfiles verticales en T y L de aluminio extruido, lacados en negro, garantizando
-          resistencia y durabilidad.
-          <br />
-          Anclaje expansivo inoxidable para fijación en forjados.
-          <br />
-          Anclaje con taco de nylon y tornillo para muros de ladrillo.
-          <br />
-          Tornillería auto-taladrante con arandela de EPDM de acero inoxidable, asegurando
-          máxima sujeción.
-          <br /> Tornillería auto-taladrante de cabeza plana lacada en acero inoxidable para
-          una fijación discreta y resistente.
-          <br />
-          <br />
-          <strong>Fijaciones:</strong>
-          <br />
-          Fijación mecánica con grapa oculta lacada en acero inoxidable con una salida de
-          5,6 mm.
-          <br />
-          Fijación química con adhesivo estructural SikaTack Panel, asegurando una unión
-          fuerte y duradera.
-          <br />
-          <br />
-          <strong>Otros componentes:</strong>
-          <br /> Imprimación Sika Primer 210T, optimizando la adherencia del adhesivo.
-          <br /> Cinta adhesiva de doble cara, mejorando la fijación inicial y facilitando
-          la instalación.
-        </Text>
-      </div>
-      <VisualizadorNota {...propsVisualizador} />
-      <Text>
-        {/* <strong>Imagen Exterior G10(Arquitecto )</strong>
+	return (
+		<StyledDolcker id={id} backgroundColor={COLORS.gray03}>
+			<Titulo>Dolcker T5 XL PORCELÁNICO soluciona</Titulo>
+			<div className="dolcker-info">
+				<ImageWithCaption
+					src={img2}
+					alt="Imagen Dolcker 2"
+					caption="Sistema T5.
+"
+				/>
+				<Text>
+					Dolcker T5 XL Porcelánico: innovación y diseño para fachadas
+					ventiladas de última generación.
+					<br />
+					<br />
+					El <strong>Sistema DOL-T5 XL</strong> es una solución avanzada para
+					fachadas ventiladas, diseñada para la instalación de revestimientos
+					cerámicos de gran formato con un sistema de fijación híbrido,
+					combinando grapa oculta y adhesivo estructural. Su diseño optimizado
+					garantiza un montaje ligero, seguro y eficiente, utilizando materiales
+					de la más alta calidad. Características Principales
+					<br />
+					<br />
+					<strong>Sistema de fijación:</strong>
+					<br />
+					Grapa oculta para una estética limpia y uniforme.
+					<br />
+					Compatible con cerámica de 3,5 - 5,6 - 10,5 - 12 mm de espesor.
+					<br />
+					Juntas horizontales de 5-6 mm y juntas verticales de 2 mm o más,
+					asegurando estabilidad y estética.
+					<br />
+					<br />
+					<strong>Anclaje y perfilería:</strong>
+					<br />
+					Ménsulas simples y dobles en L de aluminio para una fijación robusta.
+					<br />
+					Perfiles verticales en T y L de aluminio extruido, lacados en negro,
+					garantizando resistencia y durabilidad.
+					<br />
+					Anclaje expansivo inoxidable para fijación en forjados.
+					<br />
+					Anclaje con taco de nylon y tornillo para muros de ladrillo.
+					<br />
+					Tornillería auto-taladrante con arandela de EPDM de acero inoxidable,
+					asegurando máxima sujeción.
+					<br /> Tornillería auto-taladrante de cabeza plana lacada en acero
+					inoxidable para una fijación discreta y resistente.
+					<br />
+					<br />
+					<strong>Fijaciones:</strong>
+					<br />
+					Fijación mecánica con grapa oculta lacada en acero inoxidable con una
+					salida de 5,6 mm.
+					<br />
+					Fijación química con adhesivo estructural SikaTack Panel, asegurando
+					una unión fuerte y duradera.
+					<br />
+					<br />
+					<strong>Otros componentes:</strong>
+					<br /> Imprimación Sika Primer 210T, optimizando la adherencia del
+					adhesivo.
+					<br /> Cinta adhesiva de doble cara, mejorando la fijación inicial y
+					facilitando la instalación.
+				</Text>
+			</div>
+			<VisualizadorNota {...propsVisualizador} />
+			<Text>
+				{/* <strong>Imagen Exterior G10(Arquitecto )</strong>
         <br />
         -Esquina {'( dibujo técnico/croquis por parte del arquitecto)'}
         <br />
@@ -96,9 +98,9 @@ const Dolcker = ({ id }) => {
         -Arranque {'( dibujo técnico/croquis por parte del arquitecto)'}
         <br />
         -Fachada {'( dibujo técnico/croquis por parte del arquitecto)'} */}
-      </Text>
-    </StyledDolcker>
-  );
+			</Text>
+		</StyledDolcker>
+	);
 };
 
 export default Dolcker;
--- src/modules/T5/05_Diseno/index.jsx
+++ src/modules/T5/05_Diseno/index.jsx
@@ -1,141 +1,135 @@
-import Cita from '../../../components/Cita';
-import ImageWithCaption from '../../../components/ImageWithCaption';
-import { Titulo, Titulo2 } from '../../../components/Titulos';
-import { COLORS } from '../../../global/GlobalStyles';
-import StyledDisenos from './styles';
-import img1 from '../../../assets/images/T5/Diseno/catalogo3-1-14.jpg';
-import fondo from '../../../assets/images/Diseno/Formato/fondo.webp';
-import indicadoresFormato from '../../../utils/indicadoresFormato';
-import Anotacion from '../../../components/Anotacion';
-import VisualizadorVariaciones from '../../../components/VisualizadorVariaciones';
-import Anotaciones from '../../../components/Anotacion/Anotaciones';
-import VisualizadorRotador from '../../../components/VisualizadorRotador';
-import config from '../../../configs/configPiezasEspeciales';
-import configAcabados from '../../../configs/configAcabados';
-import Series from './05_2_Series';
-import ranurado11 from '../../../assets/images/DolckerTline/ranurado11mm.webp';
-import ranurado14 from '../../../assets/images/DolckerTline/ranurado11mm.webp';
-import ranurado20 from '../../../assets/images/DolckerTline/ranurado11mm.webp';
-import VisualizadorDesplazador from '../../../components/VisualizadorDesplazador';
-import Text from '../../../components/Text';
-import IndiceSeries from './05_1_IndiceSeries';
-import ConfiguradorImg from '../../../assets/images/DolckerTline/formatoscolor.webp';
-import rotadorImg from '../../../assets/images/DolckerTline/config.jpg';
+import Cita from "../../../components/Cita";
+import ImageWithCaption from "../../../components/ImageWithCaption";
+import { Titulo, Titulo2 } from "../../../components/Titulos";
+import { COLORS } from "../../../global/GlobalStyles";
+import StyledDisenos from "./styles";
+import img1 from "../../../assets/images/T5/Diseno/catalogo3-1-14.jpg";
+import fondo from "../../../assets/images/Diseno/Formato/fondo.webp";
+import indicadoresFormato from "../../../utils/indicadoresFormato";
+import Anotacion from "../../../components/Anotacion";
+import VisualizadorVariaciones from "../../../components/VisualizadorVariaciones";
+import Anotaciones from "../../../components/Anotacion/Anotaciones";
+import VisualizadorRotador from "../../../components/VisualizadorRotador";
+import config from "../../../configs/configPiezasEspeciales";
+import configAcabados from "../../../configs/configAcabados";
+import Series from "./05_2_Series";
+import ranurado11 from "../../../assets/images/DolckerTline/ranurado11mm.webp";
+import ranurado14 from "../../../assets/images/DolckerTline/ranurado11mm.webp";
+import ranurado20 from "../../../assets/images/DolckerTline/ranurado11mm.webp";
+import VisualizadorDesplazador from "../../../components/VisualizadorDesplazador";
+import Text from "../../../components/Text";
+import IndiceSeries from "./05_1_IndiceSeries";
+import ConfiguradorImg from "../../../assets/images/DolckerTline/formatoscolor.webp";
+import rotadorImg from "../../../assets/images/DolckerTline/config.jpg";
 
 const Disenos = ({ id }) => {
-  const propsFormato = {
-    anotacion: {
-      text: 'Pulsa en el punto para ver las posibilidades de formato',
-      type: 'click',
-    },
-    visualizador: {
-      config: indicadoresFormato,
-      srcfondo: fondo,
-      altFondo: 'Imagen de fondo con indicadores',
-    },
-  };
+	const propsFormato = {
+		anotacion: {
+			text: "Pulsa en el punto para ver las posibilidades de formato",
+			type: "click",
+		},
+		visualizador: {
+			config: indicadoresFormato,
+			srcfondo: fondo,
+			altFondo: "Imagen de fondo con indicadores",
+		},
+	};
 
-  return (
-    <>
-      <StyledDisenos id={id[0]} backgroundColor={COLORS.gray02}>
-        <Titulo>Diseño</Titulo>
-        <ImageWithCaption
-          src={img1}
-          alt='Imagen de Diseños 1'
-          caption='Arquitectos pasan la referencia'
-          columnSpan='6'
-        />
-        <Cita
-          cita='La arquitectura debe hablar de su tiempo y lugar, pero anhelar la eternidad'
-          author='Frank Gehry'
-          colorAutor={COLORS.gray05}
-        />
-      </StyledDisenos>
-      <StyledDisenos id={id[1]} backgroundColor={COLORS.gray01}>
-        <Titulo2>Formato</Titulo2>
-        <Anotacion {...propsFormato.anotacion} />
-        {/* <VisualizadorVariaciones {...propsFormato.visualizador} /> */}
-        <ImageWithCaption
-          src={ConfiguradorImg}
-          alt='Imagen de Diseños 1'
-          caption='Configurador interactivo'
-          columnSpan='6'
-        />
-      </StyledDisenos>
-      <StyledDisenos id={id[3]} backgroundColor={COLORS.gray01}>
-        <Titulo2>Acabados Cerámica</Titulo2>
-        <Anotaciones>
-          <Anotacion
-            type='normal'
-            text='Pulsa en el punto para ver todas las posibilidades de piezas especiales'
-          />
-          <Anotacion
-            type='swipe'
-            text='Desliza con el ratón o el dedo para apreciar la textura'
-          />
-        </Anotaciones>
-        <VisualizadorDesplazador config={configAcabados} />
-        {/* <ImageWithCaption
-                    src={interactivoImg}
-                    alt='Imagen de Diseños 1'
-                    caption='Visualizador acabados'
-                    columnSpan='6'
-                /> */}
-      </StyledDisenos>
-      <StyledDisenos id={id[4]} backgroundColor={COLORS.gray01} isEspesores>
-        <Titulo2>Espesores</Titulo2>
-        <div className='espesor espesor-1'>
-          <img src={ranurado11} alt='' />
-          <ul>
-            <strong>TC 9 / 9 mm.</strong>
-            <li>Suelo</li>
-            <li>Techo</li>
-            <li>Fachada</li>
-            <li>Recerco</li>
-          </ul>
-        </div>
-        <div className='espesor espesor-2'>
-          <img src={ranurado14} alt='' />
-          <ul>
-            <strong>TC 10 / 10 mm.</strong>
-            <li>Suelo</li>
-            <li>Techo</li>
-            <li>Fachada</li>
-            <li>Recerco</li>
-          </ul>
-        </div>
-        <div className='espesor espesor-3'>
-          <img src={ranurado20} alt='' />
-          <ul>
-            <strong>TC 11 / 11 mm.</strong>
-            <li>Suelo</li>
-            <li>Techo</li>
-            <li>Fachada</li>
-            <li>Recerco</li>
-          </ul>
-        </div>
-      </StyledDisenos>
-      <StyledDisenos id={id[5]} backgroundColor={COLORS.gray01}>
-        <Titulo2>Piezas especiales</Titulo2>
-        <Anotaciones>
-          <Anotacion
-            type='normal'
-            text='Pulsa en el punto para ver todas las posibilidades de piezas especiales'
-          />
-          <Anotacion
-            type='swipe'
-            text='Desliza con el ratón o el dedo para rotar la imagen 360º'
-          />
-        </Anotaciones>
-        {/* <VisualizadorRotador config={config} /> */}
-        <ImageWithCaption
-          src={rotadorImg}
-          alt='Imagen de Diseños 1'
-          caption='Jorge debe definir que piezas exactamente son'
-          columnSpan='6'
-        />
-      </StyledDisenos>
-      {/* <StyledDisenos id={id[5]} backgroundColor={COLORS.gray01}>
+	return (
+		<>
+			<StyledDisenos id={id[0]} backgroundColor={COLORS.gray02}>
+				<Titulo>Diseño</Titulo>
+				<ImageWithCaption
+					src={img1}
+					alt="Imagen de Diseños 1"
+					caption="Arquitectos pasan la referencia"
+					columnSpan="6"
+				/>
+				<Cita
+					cita="La arquitectura debe hablar de su tiempo y lugar, pero anhelar la eternidad"
+					author="Frank Gehry"
+					colorAutor={COLORS.gray05}
+				/>
+			</StyledDisenos>
+			<StyledDisenos id={id[1]} backgroundColor={COLORS.gray01}>
+				<Titulo2>Formato</Titulo2>
+				<Anotacion {...propsFormato.anotacion} />
+				{/* <VisualizadorVariaciones {...propsFormato.visualizador} /> */}
+				<ImageWithCaption
+					src={ConfiguradorImg}
+					alt="Imagen de Diseños 1"
+					caption="Configurador interactivo"
+					columnSpan="6"
+				/>
+			</StyledDisenos>
+			<StyledDisenos id={id[3]} backgroundColor={COLORS.gray01}>
+				<Titulo2>Acabados Cerámica</Titulo2>
+				<Anotaciones>
+					<Anotacion
+						type="normal"
+						text="Pulsa en el punto para ver todas las posibilidades de piezas especiales"
+					/>
+					<Anotacion
+						type="swipe"
+						text="Desliza con el ratón o el dedo para apreciar la textura"
+					/>
+				</Anotaciones>
+				<VisualizadorDesplazador config={configAcabados} />
+			</StyledDisenos>
+			<StyledDisenos id={id[4]} backgroundColor={COLORS.gray01} isEspesores>
+				<Titulo2>Espesores</Titulo2>
+				<div className="espesor espesor-1">
+					<img src={ranurado11} alt="" />
+					<ul>
+						<strong>TC 9 / 9 mm.</strong>
+						<li>Suelo</li>
+						<li>Techo</li>
+						<li>Fachada</li>
+						<li>Recerco</li>
+					</ul>
+				</div>
+				<div className="espesor espesor-2">
+					<img src={ranurado14} alt="" />
+					<ul>
+						<strong>TC 10 / 10 mm.</strong>
+						<li>Suelo</li>
+						<li>Techo</li>
+						<li>Fachada</li>
+						<li>Recerco</li>
+					</ul>
+				</div>
+				<div className="espesor espesor-3">
+					<img src={ranurado20} alt="" />
+					<ul>
+						<strong>TC 11 / 11 mm.</strong>
+						<li>Suelo</li>
+						<li>Techo</li>
+						<li>Fachada</li>
+						<li>Recerco</li>
+					</ul>
+				</div>
+			</StyledDisenos>
+			<StyledDisenos id={id[5]} backgroundColor={COLORS.gray01}>
+				<Titulo2>Piezas especiales</Titulo2>
+				<Anotaciones>
+					<Anotacion
+						type="normal"
+						text="Pulsa en el punto para ver todas las posibilidades de piezas especiales"
+					/>
+					<Anotacion
+						type="swipe"
+						text="Desliza con el ratón o el dedo para rotar la imagen 360º"
+					/>
+				</Anotaciones>
+				{/* <VisualizadorRotador config={config} /> */}
+				<ImageWithCaption
+					src={rotadorImg}
+					alt="Imagen de Diseños 1"
+					caption="Jorge debe definir que piezas exactamente son"
+					columnSpan="6"
+				/>
+			</StyledDisenos>
+			{/* <StyledDisenos id={id[5]} backgroundColor={COLORS.gray01}>
                 <Titulo2>Perfiles Cenefa</Titulo2>
                 <Anotaciones>
                     <Anotacion
@@ -149,7 +143,7 @@ const Disenos = ({ id }) => {
                 </Anotaciones>
                 <VisualizadorRotador config={config} />
             </StyledDisenos> */}
-      {/* <IndiceSeries id={id[6]} />
+			{/* <IndiceSeries id={id[6]} />
       <Series
         ids={[
           "basalt",
@@ -168,8 +162,8 @@ const Disenos = ({ id }) => {
           "volumen",
         ]}
       /> */}
-    </>
-  );
+		</>
+	);
 };
 
 export default Disenos;
