c54d1119dcefe00bda62442e81279c09b2116e3b
Enloch <Hugodlcq@gmail.com>
2025-04-10 07:55:09 +0200

    Add FachadaVentilada page with scroll-to-top functionality and section navigation

--- src/pages/catalogos/FachadaVentilada.jsx
+++ src/pages/catalogos/FachadaVentilada.jsx
@@ -1,87 +1,21 @@
-import React, { useRef, useState, useEffect, lazy, Suspense } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import MultipleOptionsProvider from "../../contexts/MultipleOptionsContext";
 import styled from "styled-components";
-import Preloader from "../../components/Preloader";
-import Loader from "../../components/Loader";
-
-// Resource tracking for lazy-loaded components
-const createLoadTracker = () => {
-  const resources = [];
-  let totalResources = 11; // Total number of lazy-loaded components
-  let loadedResources = 0;
-
-  return {
-    register: (promise) => {
-      const resource = { loading: true, promise };
-      resources.push(resource);
-
-      promise
-        .then(() => {
-          resource.loading = false;
-          loadedResources++;
-        })
-        .catch((error) => {
-          console.error("Error loading resource:", error);
-          resource.loading = false;
-          loadedResources++;
-        });
-
-      return promise;
-    },
-    getStatus: () => ({
-      loading: loadedResources < totalResources,
-      progress: Math.min(
-        Math.round((loadedResources / totalResources) * 100),
-        100
-      ),
-    }),
-  };
-};
-
-const resourceTracker = createLoadTracker();
-
-// Wrap lazy imports with resource tracker
-const Aplicaciones = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/05_Aplicaciones"))
-);
-const Caracteristicas = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/07_Caracteristicas"))
-);
-const Descargas = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/09_Descargas"))
-);
-const Disenos = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/06_Diseno"))
-);
-const Dolcker = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/03_Dolcker"))
-);
-const Final = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/10_Final"))
-);
-const Indice = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/02_Indice"))
-);
-const Introduccion = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/01_Introduccion"))
-);
-const Sistemas = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/08_Sistemas"))
-);
-const Ventajas = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/04_Ventajas"))
-);
-const Galeria = lazy(() =>
-  resourceTracker.register(import("../../modules/ES/System/11_Galeria"))
-);
-
+import {
+  Aplicaciones,
+  Caracteristicas,
+  Descargas,
+  Disenos,
+  Dolcker,
+  Final,
+  Indice,
+  Ventajas,
+  Sistemas,
+  Galeria,
+} from "../../modules";
 const FachadaVentilada = () => {
   const indiceRef = useRef();
   const [showButton, setShowButton] = useState(false);
-  const [resourceStatus, setResourceStatus] = useState({
-    loading: true,
-    progress: 0,
-  });
 
   const handleGoToIndice = () => {
     indiceRef.current.scrollIntoView({ behavior: "smooth" });
@@ -100,69 +34,52 @@ const FachadaVentilada = () => {
     };
   }, []);
 
-  // Track loading progress
-  useEffect(() => {
-    const updateProgress = () => {
-      setResourceStatus(resourceTracker.getStatus());
-
-      if (resourceTracker.getStatus().loading) {
-        requestAnimationFrame(updateProgress);
-      }
-    };
-
-    updateProgress();
-  }, []);
-
   return (
-    <Suspense fallback={<Loader resourceStatus={resourceStatus} />}>
-      <MultipleOptionsProvider>
-        <Indice id="indice" indiceRef={indiceRef} />
-        <Dolcker id="dolcker" />
-        <Ventajas id="ventajas" />
-        <Aplicaciones
-          id={[
-            "aplicaciones",
-            "rehabilitacion",
-            "obra-nueva",
-            "fachadas",
-            "terrazas",
-          ]}
-        />
-        <Disenos
-          id={[
-            "diseno",
-            "formato",
-            "despiece",
-            "acabados",
-            "espesores",
-            "piezas",
-            "series",
-          ]}
-        />
-        <Caracteristicas
-          id={["caracteristicas-especiales", "datos-tecnicos"]}
-        />
-        <Sistemas
-          id={[
-            "sistemas",
-            "dolcker-and-clip",
-            "dol-tc11",
-            "dol-tc14",
-            "dol-hc20",
-          ]}
-        />
-        <Galeria id="galeria" />
-        <Descargas id="descargas" />
-        <Final />
-        {showButton && (
-          <StyledButton onClick={handleGoToIndice}>
-            <span role="img" aria-label="up arrow">
-              &#8593;
-            </span>
-          </StyledButton>
-        )}
-      </MultipleOptionsProvider>
-    </Suspense>
+    <MultipleOptionsProvider>
+      <Indice id="indice" indiceRef={indiceRef} />
+      <Dolcker id="dolcker" />
+      <Ventajas id="ventajas" />
+      <Aplicaciones
+        id={[
+          "aplicaciones",
+          "rehabilitacion",
+          "obra-nueva",
+          "fachadas",
+          "terrazas",
+        ]}
+      />
+      <Disenos
+        id={[
+          "diseno",
+          "formato",
+          "despiece",
+          "acabados",
+          "espesores",
+          "piezas",
+          "series",
+        ]}
+      />
+      <Caracteristicas id={["caracteristicas-especiales", "datos-tecnicos"]} />
+      <Sistemas
+        id={[
+          "sistemas",
+          "dolcker-and-clip",
+          "dol-tc11",
+          "dol-tc14",
+          "dol-hc20",
+        ]}
+      />
+      <Galeria id="galeria" />
+      <Descargas id="descargas" />
+      <Final />
+      {showButton && (
+        <StyledButton onClick={handleGoToIndice}>
+          <span role="img" aria-label="up arrow">
+            &#8593;
+          </span>
+        </StyledButton>
+      )}
+    </MultipleOptionsProvider>
   );
 };
 
