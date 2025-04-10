7cfaa8b102f40be83342e67dcf12d13e39ae88da
Enloch <Hugodlcq@gmail.com>
2025-04-10 14:42:28 +0200

    Add TXT systems section with construction details and technical specifications

--- src/modules/TXT/07_Sistemas/index.jsx
+++ src/modules/TXT/07_Sistemas/index.jsx
@@ -41,7 +41,11 @@ const SistemasTline = ({ id }) => {
 
   return (
     <>
-      <Modal isOpen={selectedImage !== null} onClose={closeModal} caption={selectedCaption}>
+      <Modal
+        isOpen={selectedImage !== null}
+        onClose={closeModal}
+        caption={selectedCaption}
+      >
         <img src={selectedImage} alt="Ampliada" />
       </Modal>
 
@@ -98,42 +102,47 @@ const SistemasTline = ({ id }) => {
         </Enlaces>
       </StyledSistemas>
       <StyledSistemas id={id[1]}>
-				<Titulo2>TXT-11</Titulo2>
-				<Text>
-        Fachadas con alineaciones de revestimiento diferenciadas, destacando una junta vertical lacada en el color elegido por el diseñador.
-				</Text>
-				<ImageWithCaption
-					src={txt11}
-					alt="Dolcker-clip demostración"
-					columnSpan="6"
-					caption="Detalles tecnicos TXT-11"
-				/>
-			</StyledSistemas>
+        <Titulo2>TXT-11</Titulo2>
+        <Text>
+          Fachadas con alineaciones de revestimiento diferenciadas, destacando
+          una junta vertical lacada en el color elegido por el diseñador.
+        </Text>
+        <ImageWithCaption
+          src={txt11}
+          alt="Dolcker-clip demostración"
+          columnSpan="6"
+          caption="Detalles tecnicos TXT-11"
+        />
+      </StyledSistemas>
       <StyledSistemas id={id[2]}>
-				<Titulo2>TXT-12</Titulo2>
-				<Text>
-        Fachadas diseñadas para resaltar volúmenes distintivos.</Text>
-				<ImageWithCaption
-					src={txt12}
-					alt="Dolcker-clip demostración"
-					columnSpan="6"
-					caption="Detalles tecnicos TXT-12"
-				/>
-			</StyledSistemas>
+        <Titulo2>TXT-12</Titulo2>
+        <Text>Fachadas diseñadas para resaltar volúmenes distintivos.</Text>
+        <ImageWithCaption
+          src={txt12}
+          alt="Dolcker-clip demostración"
+          columnSpan="6"
+          caption="Detalles tecnicos TXT-12"
+        />
+      </StyledSistemas>
       <StyledSistemas id={id[3]}>
-				<Titulo2>TXT-13</Titulo2>
-				<Text>
-        Fachadas que incorporan perfiles estratégicos para generar sombras y potenciar la volumetría.
-				</Text>
-				<ImageWithCaption
-					src={txt13}
-					alt="Dolcker-clip demostración"
-					columnSpan="6"
-					caption="Detalles tecnicos TXT-13"
-				/>
-			</StyledSistemas>
+        <Titulo2>TXT-13</Titulo2>
+        <Text>
+          Fachadas que incorporan perfiles estratégicos para generar sombras y
+          potenciar la volumetría.
+        </Text>
+        <ImageWithCaption
+          src={txt13}
+          alt="Dolcker-clip demostración"
+          columnSpan="6"
+          caption="Detalles tecnicos TXT-13"
+        />
+      </StyledSistemas>
       <StyledSistemas id={id[4]}>
-        <Titulo2>TXT-TC14<br />Detalles esquina</Titulo2>
+        <Titulo2>
+          TXT-TC14
+          <br />
+          Detalles esquina
+        </Titulo2>
         <StyledGrid>
           <ImageWithCaption
             src={esquinaTC14_0_0}
@@ -180,7 +189,11 @@ const SistemasTline = ({ id }) => {
         </StyledGrid>
       </StyledSistemas>
       <StyledSistemas id={id[5]}>
-        <Titulo2>TXT-TC14<br />Detalles recerco</Titulo2>
+        <Titulo2>
+          TXT-TC14
+          <br />
+          Detalles recerco
+        </Titulo2>
         <StyledGrid>
           <ImageWithCaption
             src={recercoTC14_0}
@@ -206,46 +219,49 @@ const SistemasTline = ({ id }) => {
         </StyledGrid>
       </StyledSistemas>
       <StyledSistemas id={id[6]}>
-        <Titulo2>TXT-TC14<br />Leyenda</Titulo2>
-       
-          <LegendList>
-            <p data-number="01">Muro de obra o de hormigón</p>
-            <p data-number="02">Recubrimiento</p>
-            <p data-number="03">Aislamiento</p>
-            <p data-number="04">Cámara deaire ventilada</p>
-            <p data-number="05">Perfil T vertical</p>
-            <p data-number="06">Ménsula</p>
-            <p data-number="07">Taco de Nylon</p>
-            <p data-number="08">Tornillo autotaladrante</p>
-            <p data-number="09">Cerámica Dolcker</p>
-            <p data-number="10">Perfil Dolcker cuadrado</p>
-            <p data-number="11">Perfil Dolcker TXT</p>
-            <p data-number="12">Tornillo autotaladrante cabeza plana</p>
-            <p data-number="13">Fijación química</p>
-            <p data-number="14">Anclaje expansivo INOX</p>
-            <p data-number="15">Perfil Dolcker terminación</p>
-            <p data-number="16">Lámina Kerdi</p>
-            <p data-number="17">Arandela</p>
-            <p data-number="18">Panel Composite</p>
-            <p data-number="19">Cerámica</p>
-            <p data-number="20">Media grapa</p>
-            <p data-number="21">Grapa</p>
-            <p data-number="22">Sikatack panel</p>
-            <p data-number="23">Cinta doble cara</p>
-            <p data-number="24">Angular</p>
-            <p data-number="25">Taco clavo</p>
-            <p data-number="26">Extrusionado</p>
-            <p data-number="27">Sellado ventana</p>
-            <p data-number="28">Rejilla Aluminio perforada</p>
-            <p data-number="29">Lámina impermeabilizante</p>
-            <p data-number="30">Chapa de Aluminio plegada lacada</p>
-            <p data-number="31">Perfil F extrusionado</p>
-            <p data-number="32">Separador</p>
-            <p data-number="33">Poliestireno expandido</p>
-            <p data-number="34">Tornillo autotaladrante cabeza plana</p>
-            <p data-number="35">Cenefa decorativa</p>
-          </LegendList>
-       
+        <Titulo2>
+          TXT-TC14
+          <br />
+          Leyenda
+        </Titulo2>
+
+        <LegendList>
+          <p data-number="01">Muro de obra o de hormigón</p>
+          <p data-number="02">Recubrimiento</p>
+          <p data-number="03">Aislamiento</p>
+          <p data-number="04">Cámara deaire ventilada</p>
+          <p data-number="05">Perfil T vertical</p>
+          <p data-number="06">Ménsula</p>
+          <p data-number="07">Taco de Nylon</p>
+          <p data-number="08">Tornillo autotaladrante</p>
+          <p data-number="09">Cerámica Dolcker</p>
+          <p data-number="10">Perfil Dolcker cuadrado</p>
+          <p data-number="11">Perfil Dolcker TXT</p>
+          <p data-number="12">Tornillo autotaladrante cabeza plana</p>
+          <p data-number="13">Fijación química</p>
+          <p data-number="14">Anclaje expansivo INOX</p>
+          <p data-number="15">Perfil Dolcker terminación</p>
+          <p data-number="16">Lámina Kerdi</p>
+          <p data-number="17">Arandela</p>
+          <p data-number="18">Panel Composite</p>
+          <p data-number="19">Cerámica</p>
+          <p data-number="20">Media grapa</p>
+          <p data-number="21">Grapa</p>
+          <p data-number="22">Sikatack panel</p>
+          <p data-number="23">Cinta doble cara</p>
+          <p data-number="24">Angular</p>
+          <p data-number="25">Taco clavo</p>
+          <p data-number="26">Extrusionado</p>
+          <p data-number="27">Sellado ventana</p>
+          <p data-number="28">Rejilla Aluminio perforada</p>
+          <p data-number="29">Lámina impermeabilizante</p>
+          <p data-number="30">Chapa de Aluminio plegada lacada</p>
+          <p data-number="31">Perfil F extrusionado</p>
+          <p data-number="32">Separador</p>
+          <p data-number="33">Poliestireno expandido</p>
+          <p data-number="34">Tornillo autotaladrante cabeza plana</p>
+          <p data-number="35">Cenefa decorativa</p>
+        </LegendList>
       </StyledSistemas>
     </>
   );
