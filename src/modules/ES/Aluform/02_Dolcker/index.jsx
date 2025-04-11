db58d818cbecc468b1160c6ab143346026cf28c3
Enloch <Hugodlcq@gmail.com>
2025-04-11 07:51:57 +0200

    Add Dolcker Aluform and Matrix Facade system components with images and descriptions

--- src/modules/ES/Aluform/02_Dolcker/index.jsx
+++ src/modules/ES/Aluform/02_Dolcker/index.jsx
@@ -2,7 +2,7 @@ import ImageWithCaption from "../../../../components/ImageWithCaption";
 import { Titulo } from "../../../../components/Titulos";
 import StyledDolcker from "./styles";
 import { COLORS } from "../../../../global/GlobalStyles";
-import img2 from "../../../../assets/images/DolckerMatrix/Dolcker/catalogo3-1-5.jpg";
+import img2 from "../../../../assets/images/Aluform/Dolcker/IMG_7884.jpg";
 import img3 from "../../../../assets/images/Aluform/Dolcker/IMG_7677.jpg";
 import VisualizadorNota from "../../../../components/Tline/VisualizadorNota";
 import Text from "../../../../components/Text";
