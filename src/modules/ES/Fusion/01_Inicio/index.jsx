717f6d94f15da90e5220a63aa6dac6e986459aa0
Enloch <Hugodlcq@gmail.com>
2025-08-04 14:40:23 +0200

    feat: add index component with language selector and animated scroll arrow for Fusion module

--- src/modules/ES/Fusion/01_Inicio/index.jsx
+++ src/modules/ES/Fusion/01_Inicio/index.jsx
@@ -5,6 +5,7 @@ import { StyledIndice } from "./styles";
 import { BsMouse } from "react-icons/bs";
 import scrollTo from "../../../../helpers/scrollTo";
 import useMenuDesplegable from "../../../../hooks/useMenuDesplegable";
+import { Link } from "react-router-dom";
 import styled, { keyframes } from "styled-components";
 // Define la animación para la flecha
 const pulse = keyframes`
@@ -20,44 +21,45 @@ const pulse = keyframes`
 `;
 // Define los estilos para la flecha animada
 const AnimatedArrow = styled(BsMouse)`
-  animation: ${pulse} 2s ease-in-out infinite;
+	animation: ${pulse} 2s ease-in-out infinite;
 `;
 const Indice = ({ indiceRef }) => {
-  const { menuDesplegable, btnIndice } = useMenuDesplegable(
-    ListaContenidos,
-    "Índice",
-    indiceRef
-  );
+	const { menuDesplegable, btnIndice } = useMenuDesplegable(
+		ListaContenidos,
+		"Índice",
+		indiceRef
+	);
 
-  const handleClickFlecha = () => {
-    const position =
-      indiceRef.current.offsetHeight + indiceRef.current.offsetTop;
-    scrollTo(position, 2000);
-  };
+	const handleClickFlecha = () => {
+		const position =
+			indiceRef.current.offsetHeight + indiceRef.current.offsetTop;
+		scrollTo(position, 2000);
+	};
 
-  return (
-    <>
-      <StyledIndice backgroundColor={COLORS.gray06} ref={indiceRef} id="indice">
-        <Titulo color={COLORS.gray01}>Dolcker Fusion</Titulo>
-        <ListaContenidos />
-        <AnimatedArrow
-          size={32}
-          color="white"
-          alt=""
-          className="flecha-indice"
-          onClick={handleClickFlecha}
-        />
-        {/* <div className='idiomas'>
-          <Link to='/dolcker-system' className='seleccionado'>
-            ES
-          </Link>
-          <Link to='/en/dolcker-system'>EN</Link>
-        </div> */}
-      </StyledIndice>
-      {menuDesplegable}
-      {btnIndice}
-    </>
-  );
+	return (
+		<>
+			<StyledIndice backgroundColor={COLORS.gray06} ref={indiceRef} id="indice">
+				<Titulo color={COLORS.gray01}>Dolcker ARCHIFORM</Titulo>
+				<ListaContenidos />
+				<AnimatedArrow
+					size={32}
+					color="white"
+					alt=""
+					className="flecha-indice"
+					onClick={handleClickFlecha}
+				/>
+				<div className="idiomas">
+					<Link to="/dolcker-fusion" className="seleccionado">
+						ES
+					</Link>
+					<Link to="/en/dolcker-fusion">EN</Link>
+					<Link to="/fr/dolcker-fusion">FR</Link>
+				</div>
+			</StyledIndice>
+			{menuDesplegable}
+			{btnIndice}
+		</>
+	);
 };
 
 export default Indice;
--- src/modules/ES/Fusion/01_Inicio/styles.jsx
+++ src/modules/ES/Fusion/01_Inicio/styles.jsx
@@ -3,39 +3,39 @@ import Seccion from "../../../../components/Seccion";
 import { TextMedium } from "../../../../global/GlobalStyles";
 import { mediaQueryTablet } from "../../../../styles/sizes";
 const StyledIndice = styled(Seccion)`
-  min-height: 100vh;
-  .flecha-indice {
-    width: 25px;
-    grid-column: 6;
-    align-self: flex-end;
-    justify-self: flex-end;
-    cursor: pointer;
-    ${mediaQueryTablet(css`
-      display: none;
-    `)}
-  }
+	min-height: 100vh;
+	.flecha-indice {
+		width: 25px;
+		grid-column: 5;
+		align-self: flex-end;
+		cursor: pointer;
+		${mediaQueryTablet(css`
+			display: none;
+		`)}
+	}
 
-  .idiomas {
-    align-self: flex-start;
+	.idiomas {
+		align-self: flex-start;
 
-    grid-column: 12;
-    grid-row: 1;
-    display: flex;
-    ${TextMedium}
+		grid-column: 12;
+		grid-row: 1;
+		display: flex;
+		gap: 1rem;
+		${TextMedium}
 
-    a {
-      color: white;
-      text-decoration: none;
-    }
+		a {
+			color: white;
+			text-decoration: none;
+		}
 
-    .seleccionado {
-      font-weight: bold;
-    }
+		.seleccionado {
+			font-weight: bold;
+		}
 
-    ${mediaQueryTablet(css`
-      grid-column: 1 / -1;
-    `)}
-  }
+		${mediaQueryTablet(css`
+			grid-column: 1 / -1;
+		`)}
+	}
 `;
 
 export { StyledIndice };
